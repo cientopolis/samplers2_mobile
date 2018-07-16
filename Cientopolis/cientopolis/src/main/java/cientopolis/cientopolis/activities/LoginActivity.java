8dfd1c15d7681ad19ad4605beb9d3c234e910dd1
Nicolas <nico@MacBook-Pro-de-Nicolas.local>
2018-07-15 21:14:48 -0300

    logica login

--- Cientopolis/cientopolis/src/main/java/cientopolis/cientopolis/activities/LoginActivity.java
+++ Cientopolis/cientopolis/src/main/java/cientopolis/cientopolis/activities/LoginActivity.java
@@ -7,11 +7,9 @@ import android.util.Log;
 import android.view.View;
 
 import com.facebook.CallbackManager;
-import com.facebook.FacebookButtonBase;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
@@ -21,20 +19,21 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-
-import java.util.concurrent.Callable;
-
+import java.util.HashMap;
+import java.util.Map;
 import cientopolis.cientopolis.R;
+import cientopolis.cientopolis.RequestController;
+import cientopolis.cientopolis.interfaces.RequestControllerListener;
+import cientopolis.cientopolis.models.ResponseDTO;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, RequestControllerListener<String> {
 
     private static final int RC_SIGN_IN = 9001;
     private CallbackManager mFacebookCallbackManager;
     private LoginButton mFacebookSignInButton;
     private GoogleSignInClient mGoogleSignInClient;
     private SignInButton signInButton;
+    private RequestController requestController;
 
 
     @Override
@@ -46,6 +45,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
 
         mFacebookSignInButton = (LoginButton)findViewById(R.id.facebook_sign_in_button);
+        requestController = new RequestController(getApplicationContext(), this);
         signInButton = (SignInButton)findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
         signInButton.setOnClickListener(this);
@@ -58,32 +58,37 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-
-
         mFacebookSignInButton.registerCallback(mFacebookCallbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(final LoginResult loginResult) {
-                        String name = loginResult.getAccessToken().getUserId();
-                        Log.v("LOGIN-RESULT",loginResult.getAccessToken().getUserId() );
+                        Map<String, String> params = getParams();
+                        responseOk(1,null);
+                        //requestController.get(new TypeToken<ResponseDTO<String>>() {}.getType(), "login/", 1, params);
+
                     }
                     @Override
                     public void onCancel() {
-
+                        responseError(1,null);
                     }
                     @Override
                     public void onError(FacebookException error) {
-
+                        responseError(1,null);
                     }
                 }
         );
+    }
+
+    private Map<String, String> getParams(){
+        Map<String, String> params = new HashMap<>();
+        return params;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         // Check for existing Google Sign In account, if the user is already signed in
-// the GoogleSignInAccount will be non-null.
+        // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
     }
 
@@ -99,6 +104,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 GoogleSignInAccount account = result.getSignInAccount();
                 Log.v("LOGIN-RESULT",account.getDisplayName());
                 Log.v("TOKEN-ID",account.getIdToken());
+                Map<String, String> params = getParams();
+                responseOk(2,null);
+                //requestController.get(new TypeToken<ResponseDTO<String>>() {}.getType(), "login/", 2, params);
             }
         }
 
@@ -119,16 +127,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            String name = account.getDisplayName();
-            Log.v("LOGIN-RESULT",account.getDisplayName() );
-            // Signed in successfully, show authenticated UI.
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.v("EXCEPTION","signInResult:failed code=" + e.getStatusCode() );
-        }
+    @Override
+    public void responseOk(Integer id, ResponseDTO<String> response) {
+        //TODO: guardar datos en sharedPreferences
+        Intent result = new Intent();
+        setResult(1, result);
+        finish();
+
+    }
+
+    @Override
+    public void responseError(Integer id, ResponseDTO<String> response) {
+        //TODO: error response
+        Intent result = new Intent();
+        setResult(2, result);
+        finish();
     }
 }
